87d734d8ae53071b86485455c62e4cc784bdf39f
Assaf Amitai <87387953+assafam@users.noreply.github.com>
2021-10-26 11:45:55 +0300

    Registered models in admin form

--- commerce/auctions/admin.py
+++ commerce/auctions/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Auction, Bid, Comment, User
+
+class AuctionAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "description", "starting_bid", "category", "created_by", "creation_time")
+    filter_horizontal = ("watchers", )
+
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Category)
+admin.site.register(Auction, AuctionAdmin)
+admin.site.register(Bid)
+admin.site.register(Comment)
