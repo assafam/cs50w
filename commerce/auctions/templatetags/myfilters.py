df07c6dc3437cbfffa52e28fc42d6d65175ea009
Assaf Amitai <87387953+assafam@users.noreply.github.com>
2021-11-08 09:45:56 +0200

    Removed default filter name

--- commerce/auctions/templatetags/myfilters.py
+++ commerce/auctions/templatetags/myfilters.py
@@ -1,6 +1,6 @@
 from django import template
 register = template.Library()
 
-@register.filter(name='addclass')
+@register.filter
 def addclass(field, arg):
     return field.as_widget(attrs={"class": arg})
