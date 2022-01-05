3a1db9d4f5bd233e4609582f04ea0f63b92209c3
Assaf Amitai <87387953+assafam@users.noreply.github.com>
2022-01-05 15:18:40 +0200

    Create post model, add followers field

--- project4/network/models.py
+++ project4/network/models.py
@@ -3,4 +3,14 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    following = models.ManyToManyField("self", blank=True, related_name="followers")
+
+
+class Post(models.Model):
+    text = models.CharField(max_length=280)
+    creation_time = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    likes = models.ManyToManyField(User, blank=True)
+
+    def __str__(self):
+        return f"{self.text} created by: {self.user} at: {self.creation_time}"
